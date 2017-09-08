39eb265f72fc017b0b6fca7f6d84c8b98202c657
Deepak Pal <paldeepak.itm@gmail.com>
2017-09-08 17:47:17 +0530

    cucumber code

---
+++ src/runner/FireCucumber.java
@@ -0,0 +1,20 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Feature")
+public class FireCucumber {
+	public static void main(String args[])
+	{
+		
+	}
+	
+	
+
+}
